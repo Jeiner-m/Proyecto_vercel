ec47b0ecf4820613ca0596f300cfc6092d24c481
Jeiner_morelos <jeiner1724@gmail.com>
2025-05-30 17:30:41 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <p>Prueba de ejecucion de proyecto en vercel</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
